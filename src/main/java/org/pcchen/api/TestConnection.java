8cf024984cfbeb5e333db75ed3844b66a7dae28a
ceek <chenpeichao@pdmi.cn>
2020-05-22 16:04:44 +0800

    hbase测试连接

--- src/main/java/org/pcchen/api/TestConnection.java
+++ src/main/java/org/pcchen/api/TestConnection.java
@@ -1,17 +1,15 @@
 package org.pcchen.api;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.MasterNotRunningException;
-import org.apache.hadoop.hbase.ZooKeeperConnectionException;
-import org.apache.hadoop.hbase.client.Connection;
-import org.apache.hadoop.hbase.client.ConnectionFactory;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.*;
+import org.apache.hadoop.hbase.client.*;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * 测试连接类
+ * 获取不到bdtest-02：注意修改本地hosts中ip映射
  *
  * @author ceek
  * @create 2020-05-19 16:22
@@ -19,20 +17,29 @@ import java.io.IOException;
 public class TestConnection {
     public static void main(String[] args) throws IOException {
         Configuration conf = HBaseConfiguration.create();
-        conf.set("hbase.zookeeper.quorum", "10.10.32.59");
+
+        //可以通过conf代码指定，也可以通过在classpath目录下添加hbase-site.xml文件中指定
+        conf.set("hbase.zookeeper.quorum", "10.10.32.61");
         conf.set("hbase.zookeeper.property.clientPort", "2181");
+//        conf.set("hbase.master", "10.10.32.61:60000");
         Connection connection = ConnectionFactory.createConnection(conf);
         HBaseAdmin admin = (HBaseAdmin) connection.getAdmin();
 
-        boolean student = admin.tableExists("student");
-        Configuration configuration = admin.getConfiguration();
-        System.out.println(configuration.toString());
-    }
+        Table student = connection.getTable(TableName.valueOf("student"));
+        //查询t01表中所有行数据
+        Scan scan = new Scan();
+        ResultScanner result = student.getScanner(scan);//得到的是数据集
+        Result itemResult = null;
+        while ((itemResult = result.next()) != null) {
+            List<Cell> cells = itemResult.listCells();//将每条数据存到Cell对象里
+            for (Cell cell : cells) {
+                System.out.println(new String(CellUtil.cloneFamily(cell)) + new String(CellUtil.cloneQualifier(cell)) + new String(CellUtil.cloneValue(cell)));
+            }
+        }
 
-    public static boolean isTableExist(String tableName) throws MasterNotRunningException, ZooKeeperConnectionException, IOException {
-        //在HBase中管理、访问表需要先创建HBaseAdmin对象
-        Configuration conf = HBaseConfiguration.create();
-        HBaseAdmin admin = new HBaseAdmin(conf);
-        return admin.tableExists(tableName);
+
+//        boolean studentBoolean = admin.tableExists("student");
+//        Configuration configuration = admin.getConfiguration();
+//        System.out.println(configuration.toString());
     }
 }
